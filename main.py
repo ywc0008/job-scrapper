c3ac573441ddf40c0bbc46ca0e25eb421ce2acf9
wzz <ywc0008@naver.com>
2024-01-09 05:10:37 +0900

    playwright로 페이지 이동하고 상호작용하기

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright
 import time
+from bs4 import BeautifulSoup
 
 p = sync_playwright().start()  # 초기화
 
@@ -10,7 +11,26 @@ browser = p.chromium.launch(
 page = browser.new_page()  # 브라우저 새창 열기
 
 page.goto("https://www.wanted.co.kr/jobsfeed")  # 해당 url로 이동
+time.sleep(5)
 
-time.sleep(7)
+page.click("button.Aside_searchButton__Xhqq3")
+time.sleep(5)
 
-page.screenshot(path="screenshot.png")  # 페이지 스크린샷 찍기
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+time.sleep(5)
+
+page.keyboard.down("Enter")
+time.sleep(5)
+
+page.click("a#search_tab_position")
+time.sleep(5)
+
+for x in range(4):
+    page.keyboard.down("End")
+    time.sleep(5)
+
+content = page.content()
+
+p.stop()
+
+soup = BeautifulSoup(content, "html.parser")
